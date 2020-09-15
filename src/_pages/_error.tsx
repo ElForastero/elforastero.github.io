50dadfb809ab692060a584244a7a69b570356f12
GitHub Action <action@github.com>
2020-09-15 08:30:00 +0000

    :rocket: Deploy

--- src/_pages/_error.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Error from 'next/error';
-
-const CustomErrorPage = ({ errorCode }) => {
-  return <Error statusCode={errorCode} />;
-};
-
-export default CustomErrorPage;
--- src/components/atoms/Typography/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './Heading2';
-export * from './Heading3';
